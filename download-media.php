0bb478dcc8c90630c2c13148c9d5328b97d9b798
Pau <pauiglesias@gmail.com>
2017-09-19 15:05:49 +0200

    Added main class and a readme.txt file.

---
+++ download-media.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Download Media
+Plugin URI: https://www.littlebizzy.com
+Description: Quickly and easily download a raw file of any media currently uploaded on your WordPress website without requiring SFTP info or fancy dependencies.
+Version: 1.0
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+// Avoid script calls via plugin URL
+if (!function_exists('add_action'))
+	die;
+
+// This plugin constants
+define('DWNMDA_FILE', __FILE__);
+define('DWNMDA_PATH', dirname(DWNMDA_FILE));
+define('DWNMDA_VERSION', '1.0.0');
+
+// Restricted to the admin area but not in AJAX mode
+if (!is_admin() || (defined('DOING_AJAX') && DOING_AJAX))
+	return;
+
---
+++ readme.txt
@@ -0,0 +1,10 @@
+=== Download Media ===
+Contributors: littlebizzy
+Tags: download, media
+Requires at least: 4.4
+Tested up to: 4.8
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Quickly and easily download a raw file of any media currently uploaded on your WordPress website without requiring SFTP info or fancy dependencies.
